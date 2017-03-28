1c819fa2447dece58e2fa1c59eaf76d481f838e1
wuhuajun001 <wuhuajun_001@hotmail.com>
2017-03-28 15:58:39 +0800

    MongoCache

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 # -*-coding: utf-8
 from test1 import download
 from pymongo import MongoClient
-url = 'http://sh.lianjia.com/ditiefang/li143685036s100021813/d2p22'
-conn = MongoClient('192.2.4.166',27017)
+from test1 import MongoCache
+import datetime
+
+print datetime.timedelta(minutes = 2)
+
--- test1.py
+++ test1.py
@@ -1,20 +1,22 @@
 # -*-coding: utf-8
 import re
+import json
+import hashlib
 import urllib2
-import urlparse
 import itertools
+import urlparse
 from bs4 import BeautifulSoup
-from datetime import datetime
 from pymongo import MongoClient
-from setting import db_host, db_name, db_port
+from setting import db_host, db_port
+from datetime import datetime, timedelta
+
 
 def download(url,num_retires = 2):
     print 'Downloading: ', url
-    conn = Mongo()
     record = conn.getitem(url)
     if record:
         html = record['result']
-    else:
+    if not record:
         try:
             html = urllib2.urlopen(url).read()
         except urllib2.URLError as e:
@@ -26,8 +28,38 @@ def download(url,num_retires = 2):
     conn.setitem(url, html)
     return html
 
+def crawler(links):
+    for link in links:
+        for page in itertools.count(1):
+            url = link + "/d%dp22" % page
+            html = download(url)
+            soup = BeautifulSoup(html, 'lxml')
+            house_list = soup.select('ul.house-lst > li')
+            if not house_list or soup.find(attrs={'class': 'list-no-data clear'}):
+                break
 
-def get_links(url,regex):
+            for house in house_list:
+                try:
+                    metro_list = soup.find_all(attrs={'class': 'js_condition'})
+                    msg = {
+                        'xiaoqu': house.find(attrs={'class': 'laisuzhou'}).string,
+                        'price': house.find(attrs={'class': 'num'}).string,
+                        'pingfang': house.select('div.where > span')[1].string,
+                        'per': house.find(attrs={'class': 'price-pre'}).string,
+                        'location': '%s%s' % (
+                        house.select('div.con > a')[0].string, house.select('div.con > a')[1].string),
+                        'metro': metro_list[0].string,
+                        'station': metro_list[1].string,
+                        'rights': house.select('span.taxfree-ex > span')[0].string,
+                        'createtime': '%s' % datetime.now()
+                    }
+                except:
+                    continue
+                conn.insert_house(msg)
+                print msg
+
+
+def get_links(url, regex):
     links = soup.find_all(name="a")
     link_list = []
     for link in links:
@@ -41,65 +73,54 @@ def get_links(url,regex):
             continue
     return link_list
 
-class Mongo:
-    def __init__(self,client= MongoClient('192.2.4.166', 27017)):
+class MongoCache:
+    def __init__(self, client=MongoClient(db_host, db_port), expires=timedelta(days=1)):
         self.client = client
         self.db = client.cache
+        self.db.webpage.create_index('createtime',
+            expireAfterSeconds=expires.total_seconds())
+
     def getitem(self, url):
-        self.db.webpage.find_one({'id': url})
-        # if record:
-        #     return record['result']
-        # else:
-        #
-        #     raise KeyError(url + ' dos not exist')
+        return self.db.webpage.find_one({'_id': url})
+
     def setitem(self, url, result):
-        record = {'url': url, 'result': result}
+        record = {'url': url, 'result': result, 'createtime': datetime.utcnow()}
         self.db.webpage.update({'_id': url}, {'$set': record}, upsert=True)
 
-def mongo(db_host, db_name, db_port = 27017):
-    conn = MongoClient(db_host, db_port)
+    def insert_house(self,msg):
+        hash_key = hash_value(msg)
+        self.db.house.update({'_id': hash_key}, {'$set': msg}, upsert=True)
+
+def hash_value(msg):
+    #for generating Mongo id
+    string = json.dumps(msg)
+    m = hashlib.sha1()
+    m.update(string)
+    return m.hexdigest()
+
+def multidownloader(links):
+    try:
+        url = links.pop()
+    except IndexError:
+        pass
 
 
 
 if __name__ == "__main__":
-    # for page in itertools.count(1):
-        #Line 16
+    #For All Lines
+    threads = []
+    conn = MongoCache()
     stime = datetime.now()
-    seed_url = "http://sh.lianjia.com/ditiefang/li110460733"
+    seed_url = "http://sh.lianjia.com/ditiefang/"
     html = download(seed_url)
     soup = BeautifulSoup(html,'lxml')
     regex = r'li\d+$'
     links = get_links(seed_url,regex)
-    print links
     for link in links:
         regex = r'li\d+s\d+'
         html = download(link)
         soup = BeautifulSoup(html,'lxml')
-        links= get_links(link,regex)
-        for link in links:
-            for page in itertools.count(1):
-                url = link+"/d%dp22" % page
-                html = download(url)
-                soup = BeautifulSoup(html,'lxml')
-                house_list = soup.select('ul.house-lst > li')
-                if not house_list or soup.find(attrs={'class':'list-no-data clear'}):
-                    break
-
-                for house in house_list:
-                    try:
-                        metro_list = soup.find_all(attrs={'class':'js_condition'})
-                        msg = {
-                            'xiaoqu': house.find(attrs = {'class':'laisuzhou'}).string,
-                            'price' : house.find(attrs = {'class':'num'}).string,
-                            'pingfang' : house.select('div.where > span')[1].string,
-                            'per' : house.find(attrs = {'class':'price-pre'}).string,
-                            'location' : '%s%s'%(house.select('div.con > a')[0].string,house.select('div.con > a')[1].string),
-                            'metro': metro_list[0].string,
-                            'station': metro_list[1].string,
-                            'rights' : house.select('span.taxfree-ex > span')[0].string,
-                        }
-                    except:
-                        continue
-                    print msg
+        links= get_links(link, regex)
+        crawler(links)
     time_duration = datetime.now() - stime
     print time_duration
